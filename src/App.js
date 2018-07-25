6094b7eae75474ea02dd7b9bbea5ce1e0a3c2287
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-07-25 14:17:42 +0800

    Fetch books from localhost:3000

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			books: []
+		};
+	}
+
+	async getBooks() {
+		const url = 'http://localhost:3000/books';
+		const response = await fetch(url);
+		const data = await response.json();
+		this.setState({
+			books: data
+		});
+	}
+
+	componentDidMount() {
+		this.getBooks();
+	}
+
+	render() {
+		return (
+			<div>
+				{this.state.books.map((book) => {
+					return <li key={book._id}>{book.title}</li>;
+				})}
+			</div>
+		);
+	}
 }
 
 export default App;
